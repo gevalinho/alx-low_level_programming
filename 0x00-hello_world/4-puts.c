e71c5f8edb83abff7073ff23f46b86740a2071ff
gevalinho <gevalinho1@gmail.com>
2022-10-13 16:57:16 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /**
  * main - print the string in the output function
  * Programming is like building a multilingual puzzle
- *
+ *Return: 0
  */
 int main(void)
 {	puts("\"Programming is like building a multilingual puzzle");
