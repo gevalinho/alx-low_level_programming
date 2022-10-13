0cf0edb8d6b89bdf699a6225a12d101223132883
gevalinho <gevalinho1@gmail.com>
2022-10-13 16:54:33 +0000

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the output function
+ * Programming is like building a multilingual puzzle
+ *
+ */
+int main(void)
+{	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
