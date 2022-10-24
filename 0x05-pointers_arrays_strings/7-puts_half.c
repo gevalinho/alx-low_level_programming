77d2a6563a081912aaf77cee05b98dbd53cc90b5
gevalinho <gevalinho1@gmail.com>
2022-10-24 15:46:42 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,12 +16,7 @@ void puts_half(char *str)
 
 	int len, n, i;
 
-
-
 	len = 0;
-
-
-
 	while (str[len] != '\0')
 
 	{
@@ -30,8 +25,6 @@ void puts_half(char *str)
 
 	}
 
-
-
 	if (len % 2 == 0)
 
 	{
@@ -39,27 +32,18 @@ void puts_half(char *str)
 		for (i = len / 2; str[i] != '\0'; i++)
 
 		{
-
 			_putchar(str[i]);
-
 		}
-
 	}
 
 	else if (len % 2)
 
 	{
-
 		for (n = (len - 1) / 2; n < len - 1; n++)
-
 		{
-
 			_putchar(str[n + 1]);
-
 		}
-
 	}
 
 	_putchar('\n');
-
 }
