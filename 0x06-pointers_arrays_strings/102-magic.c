473abff5779b1914d1d1fb60a97ff01a24fc6c1f
gevalinho <gevalinho1@gmail.com>
2022-10-27 13:11:24 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,35 +1,37 @@
 #include <stdio.h>
 
-/**
- * main - making margic happen
- *
- * Return: Always return 0
- */
+
 
 int main(void)
+
+
+
 {
+
 	int n;
 
 	int a[5];
 
 	int *p;
 
+
+
 	a[2] = 1024;
 
 	p = &n;
 
-	/*
-	 *    write your line of code here...
+	/**
+	 *  write your line of code here...
 	 *
-	 *    Remember:
+	 *  Remember:
 	 *
-	 *   - you are not allowed to use a
+	 *  you are not allowed to use a
 	 *
-	 *   - you are not allowed to modify p
+	 *  you are not allowed to modify p
 	 *
-	 *  - only one statement
+	 *  only one statement
 	 *
-	 *  - you are not allowed to code anything else than this line of code
+	 *  you are not allowed to code anything else than this line of code
 	 *
 	 */
 
