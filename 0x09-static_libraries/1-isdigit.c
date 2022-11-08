6b0cd20cbbaf7f28912fc61e1651c4da55fbc43a
gevalinho <gevalinho1@gmail.com>
2022-11-08 13:10:02 -0800

    libmy.a

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+/**
+ *   _isdigit - function that checks for uppercase character.
+ *
+ *   @c: the int for the paramaters of my function
+ *
+ *   Return: 0
+ *
+ */
+
+int _isdigit(int c)
+
+{
+
+	return (0);
+
+}
