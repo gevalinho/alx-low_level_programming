9a720e6ab02e8298048988c2e5b10535b98b87ae
gevalinho <gevalinho1@gmail.com>
2022-11-09 02:48:28 -0800

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,39 +20,28 @@
 int **alloc_grid(int width, int height)
 
 {
-
 	int **arr;
 
 	int i, j;
-
-
 
 	if (height <= 0 || width <= 0)
 
 		return (NULL);
 
-
-
 	arr = (int **) malloc(sizeof(int *) * height);
-
-
 
 	if (arr == NULL)
 
 		return (NULL);
 
-
-
 	for (i = 0; i < height; i++)
 
 	{
-
 		arr[i] = (int *) malloc(sizeof(int) * width);
 
 		if (arr[i] == NULL)
 
 		{
-
 			free(arr);
 
 			for (j = 0; j <= i; j++)
@@ -60,27 +49,17 @@ int **alloc_grid(int width, int height)
 				free(arr[j]);
 
 			return (NULL);
-
 		}
-
 	}
-
-
 
 	for (i = 0; i < height; i++)
 
 	{
-
 		for (j = 0; j < width; j++)
 
 		{
-
 			arr[i][j] = 0;
-
 		}
-
 	}
-
 	return (arr);
-
 }
