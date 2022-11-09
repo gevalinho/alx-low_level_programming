a4e47338c3e2b2ffb84fcb055ac73be793e63e4e
gevalinho <gevalinho1@gmail.com>
2022-11-09 02:35:18 -0800

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,62 +2,85 @@
 
 #include "main.h"
 
+
+
 /**
  *  **alloc_grid - creates a two dimensional array of ints
  *
- *  @width: width of the matrix
+ *   @width: width of the matrix
  *
- *  @height: height of the matrix
+ *   @height: height of the matrix
  *
- *  Return: pointer to the created matrix (Success)
+ *   Return: pointer to the created matrix (Success)
  *
- *  or NULL (Error)
+ *   or NULL (Error)
  *
  */
 
 int **alloc_grid(int width, int height)
 
 {
+
 	int **arr;
 
 	int i, j;
 
+
+
 	if (height <= 0 || width <= 0)
+
 		return (NULL);
 
+
+
 	arr = (int **) malloc(sizeof(int *) * height);
+
+
+
 	if (arr == NULL)
 
 		return (NULL);
 
+
+
 	for (i = 0; i < height; i++)
+
+	{
 
 		arr[i] = (int *) malloc(sizeof(int) * width);
 
-	if (arr[i] == NULL)
+		if (arr[i] == NULL)
 
-	{
+		{
 
-		free(arr);
+			free(arr);
 
-		for (j = 0; j <= i; j++)
+			for (j = 0; j <= i; j++)
 
-			free(arr[j]);
+				free(arr[j]);
 
-		return (NULL);
+			return (NULL);
+
+		}
 
 	}
 
-}
 
-for (i = 0; i < height; i++)
 
-{
-	for (j = 0; j < width; j++)
+	for (i = 0; i < height; i++)
 
 	{
-		arr[i][j] = 0;
+
+		for (j = 0; j < width; j++)
+
+		{
+
+			arr[i][j] = 0;
+
+		}
+
 	}
-}
-return (arr);
+
+	return (arr);
+
 }
