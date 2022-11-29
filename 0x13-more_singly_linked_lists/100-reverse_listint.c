c5dbf510f4b860be7275e6bfbfc6df9499e2b6be
gevalinho <gevalinho1@gmail.com>
2022-11-29 15:07:01 +0100

    100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -10,7 +10,7 @@ listint_t *reverse_listint(listint_t **head)
 listint_t *prev = NULL;
 listint_t *next = NULL;
 
-while (*head)
+while (*head != NULL)
 {
 next = (*head)->next;
 (*head)->next = prev;
