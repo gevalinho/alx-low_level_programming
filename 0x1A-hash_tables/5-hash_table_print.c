0186502a679dad0a59e433358c7baf15d00097e7
gevalinho <gevalinho1@gmail.com>
2023-03-06 08:54:55 +0100

    5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 /**
- hash_table_print - function that prints a hash table
- @ht: is the hash table
+ * hash_table_print - function that prints a hash table
+ * @ht: is the hash table
  */
 void hash_table_print(const hash_table_t *ht)
 {
