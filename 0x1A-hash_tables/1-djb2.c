ddd9f57f96d2b8b3c7aadfefc47256e744ea8b60
gevalinho <gevalinho1@gmail.com>
2023-03-06 08:36:57 +0100

    1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -11,8 +11,7 @@ unsigned long hash = 5381;
 int c;
 
 while ((c = *str++))
-hash = ((hash << 5) + hash) + c;
-/* hash * 33 + c */
+hash = ((hash << 5) + hash)+c; /* hash * 33 + c */
 
 return (hash);
 }
